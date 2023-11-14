70088675a8d7eb9079f88c3db1f32c32f4b1cbb5
chenjunze <chenjunze@xmov.ai>
2023-11-14 17:15:17 +0800

    feat: pipe validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: false,
   })
+  app.useGlobalPipes(new ValidationPipe())
   app.useStaticAssets('public', { prefix: '/static' })
   await app.listen(3000)
 }
