b4ec6402894920cd028533f618ff6c85b4b0f9ab
AvailableForTheWorld <435829792@qq.com>
2025-10-21 22:27:53 +0800

    feat: 02 hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { ParentComp4 } from './02.hooks-master'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParentComp4 />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
